261483002b743cb32cbb15a4413e9c32ea94a0d0
Xingxing Geng <xingxinggeng@umail.ucsb.edu>
2017-11-25 13:27:52 -0800

    first part

--- wtdgraph.h
+++ wtdgraph.h
@@ -13,6 +13,8 @@
 using namespace std;
 using std::size_t;
 //using std::map;
+typedef pair<size_t, size_t> Pair;
+typedef map<Pair, size_t> Map;
 
 namespace main_savitch_15{
 
@@ -20,82 +22,81 @@ template <class Item>
 class wtdgraph : public graph<Item>
 {
 	public:
-			wtdgraph() : graph<Item>(){ weights = NULL;}
+			wtdgraph() : graph<Item>(){ }
 
 			wtdgraph( size_t initial_allocation ) : graph<Item>(initial_allocation){//weights=NULL;
 			}
 
 			wtdgraph(const wtdgraph &source) : graph<Item>(source){
-				weights = new map<size_t,size_t>[source.size()];
 				for(size_t i=0;i<source.size();i++){
 					for(size_t j=0;j<source.size();j++){
+							if (i == j) continue;
 							if(source.is_edge(i,j)){
-									weights[i][j] = source.edge_weight(i,j);//no need if statement?
+									Pair ij(i,j);
+									weights[ij] = source.edge_weight(i,j);//ij is not a size_t type?
 							}
 					}
 				}
 			}
 
 			//destructor
-			virtual ~wtdgraph(){
-					if(weights != NULL){
-							delete [] weights;
-					}
+			virtual ~wtdgraph(){				
+				weights.clear();
 			}
 
 			//overload add_edge
 			void add_edge(size_t source, size_t target, size_t weight = 0){
 					graph<Item>::add_edge(source,target);
-					weights[source][target] = weight;
+					size_t i = source;
+					size_t j = target;
+					Pair ij(i,j);
+					weights[ij] = weight;
 			}
 
 			//redefine
 			void remove_edge(size_t source, size_t target){
 					graph<Item>::remove_edge(source,target);
-					weights[source][target]=0;
+					size_t i = source;
+					size_t j = target;
+					Pair ij(i,j);
+					weights.erase(ij);
 			}
 
 			wtdgraph<Item> &operator=(const wtdgraph &source){
 					if(this == &source){return *this;}
-					size_t current_allocation = size();//no need?
 					graph<Item>::operator=(source);
-					delete [] weights;
-					weights = new map<size_t,size_t>[source.size()];
+					weights.clear();//is this valid
 					for(size_t i=0;i<source.size();i++){
 							for(size_t j=0;j<source.size();j++){
+									if(i == j)continue;
 									if(source.is_edge(i,j)){
-											weights[i][j] = source.edge_weight(i,j);
+											Pair ij(i,j);
+											weights[ij] = source.edge_weight(i,j);
 									}
 							}
 					}
-			}
+				}
 
 			//override resize
 			virtual void resize(size_t new_allocation){
-					if(new_allocation <= size()){return ;}
+					if(new_allocation <=graph<Item>::size()){return ;}
 					else{
-						size_t current_allocation = size();
-						graph<Item>::resize(new_allocation);
-						map<size_t,size_t>* new_weights = new map<size_t,size_t>[new_allocation];
-						for(size_t i = 0;i < current_allocation;i++){
-								for(size_t j = 0;j < current_allocation;j++){
-										new_weights[i][j] = weights[i][j];
-								}
-						}
-						delete [] weights;
-						weights = new_weights;
+						graph<Item>::resize(new_allocation);	
 					}
 			}
 
 
 			//add new function
 			size_t edge_weight(size_t source,size_t target) const{
-					assert(is_edge(source,target));
-					return weights[source][target];
-									}
+					assert(graph<Item>::is_edge(source,target));
+					size_t i = source;
+					size_t j = target;
+					Pair ij(i,j);
+					return weights.at(ij);
+			}
 	
 	private:
-			map<size_t,size_t>* weights;
+			Map weights;
 
 
 	};
