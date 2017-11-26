53aa540f398983f1351aca35b34b1ba00e80b79a
Xingxing Geng <xingxinggeng@umail.ucsb.edu>
2017-11-26 13:51:34 -0800

    finished second part

--- paths.cpp
+++ paths.cpp
@@ -15,7 +15,7 @@ using std::string;
 const size_t INF = ULONG_MAX;
 
 void shortest(wtdgraph<string> &g,size_t start,vector<size_t> &distances,vector<list<size_t> > &paths){
-		distances.resize(g.size(),INF);
+//		distances.resize(g.size(),INF);
 		for(size_t i=0;i<g.size();++i){
 				distances[i] = INF;
 		}
@@ -30,14 +30,14 @@ void shortest(wtdgraph<string> &g,size_t start,vector<size_t> &distances,vector<
 				size_t next;
 				size_t smallest_value = INF;
 				for(size_t k=0;k<g.size();k++){
-						if(allowed_vertices.find(k) != allowed_vertices.end() && distances[k]<smallest_value){
+						if(allowed_vertices.find(k) == allowed_vertices.end() && distances[k]<smallest_value){
 								next = k;
 								smallest_value = distances[k];
 						}//find the next
 				}
 				allowed_vertices.insert(next);
 				for(size_t v = 0; v< g.size();v++){
-						if(allowed_vertices.find(v) != allowed_vertices.end() && g.is_edge(next,v)){
+						if(allowed_vertices.find(v) == allowed_vertices.end() && g.is_edge(next,v)){
 								size_t sum = distances[next] + g.edge_weight(next,v);
 								if(sum < distances[v]){
 										distances[v] = sum;
@@ -53,5 +53,6 @@ void shortest(wtdgraph<string> &g,size_t start,vector<size_t> &distances,vector<
 						vertex_on_path = predecessor[vertex_on_path];
 						paths[ve].push_front(vertex_on_path);
 				}
+				paths[ve].push_front(start);//need?
 		}
 }
